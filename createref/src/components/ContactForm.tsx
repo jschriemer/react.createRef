68a61ed0e5cf20cf12098b0bfce06e86e555989c
John <jschriem@gmail.com>
2022-11-30 12:32:55 -0800

    dynamic mobile contact form

--- createref/src/components/ContactForm.tsx
+++ createref/src/components/ContactForm.tsx
@@ -1,10 +1,6 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Form } from "react-final-form";
-import {
-  TextField,
-  Checkboxes,
-  Select,
-} from "mui-rff";
+import { TextField, Checkboxes, Select } from "mui-rff";
 import {
   Typography,
   Paper,
@@ -117,72 +113,161 @@ const formFields = [
 ];
 
 export default function ContactForm() {
-  return (
-    <div
-      style={{
-        padding: 16,
-        margin: "auto",
-        maxWidth: "65vw",
-        background: "#D9D9D9",
-        borderRadius: '5px'
-      }}
-    >
-      <CssBaseline />
-      <div style={{ display: "flex" }}>
-        <div>
-          <Typography variant="h4" align="center" component="h1" gutterBottom>
-            <span role="img" aria-label="wave">
-              👋
-            </span>{" "}
-            Lets Get in Touch!
-          </Typography>
-          <Form
-            onSubmit={onSubmit}
-            validate={validate}
-            render={({ handleSubmit, submitting }) => (
-              <form onSubmit={handleSubmit} noValidate>
-                <Paper
-                  style={{
-                    padding: 16,
-                    background: "transparent",
-                    boxShadow: "none",
-                  }}
-                >
-                  <Grid container alignItems="flex-start" spacing={2}>
-                    {formFields.map((item, idx) => (
-                      <Grid item xs={item.size} key={idx}>
-                        {item.field}
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    console.log(window.innerWidth);
+    if (window.innerWidth > 900) {
+      setIsMobile(false);
+    } else if (window.innerWidth < 900) {
+      setIsMobile(true);
+    }
+  });
+
+  useEffect(() => {
+    const handleResize = () => {
+      console.log(window.innerWidth);
+      if (window.innerWidth > 900) {
+        setIsMobile(false);
+      } else if (window.innerWidth < 900) {
+        setIsMobile(true);
+      }
+    };
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  if (isMobile) {
+    return (
+      <div
+        style={{
+          padding: 16,
+          margin: "auto",
+          background: "transparent",
+        }}
+      >
+        <CssBaseline />
+        <div style={{ display: "flex" }}>
+          <div>
+            <Typography variant="h4" align="center" component="h1" gutterBottom>
+              <span role="img" aria-label="wave">
+                👋
+              </span>{" "}
+              Lets Get in Touch!
+            </Typography>
+            <Form
+              onSubmit={onSubmit}
+              validate={validate}
+              render={({ handleSubmit, submitting }) => (
+                <form onSubmit={handleSubmit} noValidate>
+                  <Paper
+                    style={{
+                      padding: 16,
+                      background: "transparent",
+                      boxShadow: "none",
+                    }}
+                  >
+                    <Grid container alignItems="flex-start" spacing={2}>
+                      {formFields.map((item, idx) => (
+                        <Grid item xs={item.size} key={idx}>
+                          {item.field}
+                        </Grid>
+                      ))}
+                      <Grid item style={{ marginTop: 16 }}>
+                        <Button
+                          variant="contained"
+                          color="primary"
+                          type="submit"
+                          disabled={submitting}
+                        >
+                          Submit
+                        </Button>
                       </Grid>
-                    ))}
-                    <Grid item style={{ marginTop: 16 }}>
-                      <Button
-                        variant="contained"
-                        color="primary"
-                        type="submit"
-                        disabled={submitting}
-                      >
-                        Submit
-                      </Button>
                     </Grid>
-                  </Grid>
-                </Paper>
-              </form>
-            )}
-          />
-        </div>
-        <div style={{ marginLeft: "5%", padding: "5%" }}>
-          <img
-            src={require("../images/kona_care_logo.PNG")}
-            style={{ maxWidth: "300px" }}
-          />
-          <Typography variant="h5" align="center" gutterBottom color="#FFA501">
-            <p>Kona Care Landscaping </p>
-            <p>Calgary, AB Canada</p>
-            <p>konacare@gmail.com</p>
-            <p>604-123-4567</p>
-          </Typography>
+                  </Paper>
+                </form>
+              )}
+            />
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div
+        style={{
+          padding: 16,
+          margin: "auto",
+          maxWidth: "65vw",
+          background: "#D9D9D9",
+          borderRadius: "5px",
+        }}
+      >
+        <CssBaseline />
+        <div style={{ display: "flex" }}>
+          <div>
+            <Typography variant="h4" align="center" component="h1" gutterBottom>
+              <span role="img" aria-label="wave">
+                👋
+              </span>{" "}
+              Lets Get in Touch!
+            </Typography>
+            <Form
+              onSubmit={onSubmit}
+              validate={validate}
+              render={({ handleSubmit, submitting }) => (
+                <form onSubmit={handleSubmit} noValidate>
+                  <Paper
+                    style={{
+                      padding: 16,
+                      background: "transparent",
+                      boxShadow: "none",
+                    }}
+                  >
+                    <Grid container alignItems="flex-start" spacing={2}>
+                      {formFields.map((item, idx) => (
+                        <Grid item xs={item.size} key={idx}>
+                          {item.field}
+                        </Grid>
+                      ))}
+                      <Grid item style={{ marginTop: 16 }}>
+                        <Button
+                          variant="contained"
+                          color="primary"
+                          type="submit"
+                          disabled={submitting}
+                        >
+                          Submit
+                        </Button>
+                      </Grid>
+                    </Grid>
+                  </Paper>
+                </form>
+              )}
+            />
+          </div>
+          <div style={{ marginLeft: "5%", padding: "5%" }}>
+            <img
+              src={require("../images/kona_care_logo.PNG")}
+              style={{ maxWidth: "300px" }}
+            />
+            <Typography
+              variant="h5"
+              align="center"
+              gutterBottom
+              color="#FFA501"
+            >
+              <p>Kona Care Landscaping </p>
+              <p>Calgary, AB Canada</p>
+              <p>konacare@gmail.com</p>
+              <p>604-123-4567</p>
+            </Typography>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
